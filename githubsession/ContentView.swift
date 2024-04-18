c3dc87e8b34a56518887762eb801aed192f846a0
GitDohyun <gitdohyun@gmail.com>
2024-04-18 20:21:18 +0900

    Initial Commit

---
+++ githubsession/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  githubsession
+//
+//  Created by 김도현 on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ githubsession/githubsessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubsessionApp.swift
+//  githubsession
+//
+//  Created by 김도현 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct githubsessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
